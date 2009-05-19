a241e9407115d5338dd4379ac1eb22249cee5af5
Marcus Cobden <git@marcuscobden.co.uk>
2009-05-20 00:30:17 +0100

    The beginnings of a big refactor and testifying of the codebase.

--- src/tree/tree.js
+++ src/tree/tree.js
@@ -1,46 +1,100 @@
-var Tree = new Class({
-	Implements: Options,
+var CanvasTreeNode = new Class({
+	id: null, x: null, y: null,
 	
-	root: null,
-	
-	options: {
-		treeLevelClass: TreeLevel
-	},
-	initialize: function(elem, engine, options) {
-		this.setOptions(options);
-		this.elem = $(elem);
-		this.engine = engine;
-	},
-	partition: function(){
-		this.root = new (this.options.treeLevelClass)(null, 0, 0, this.elem.width, this.elem.height);
-		
-		var tmp = this.engine.getNodes();
-		var nodes = [];
-		for (var i in tmp)
-			nodes[i] = tmp[i];
-			
-		this.root.addNodes(nodes);
+	initialize: function(id, x, y) {
+		this.id = id; this.x = x; this.y = y;
 	}
 });
 
-var TreeLevel = new Class({
+var CanvasTree = new Class({
+	Implements: Options,
+	
+	root: null,
+	canvas: null,
+	node_id_to_level: {},
+	
+	options: {
+		treeLevelClass: 'CanvasQuadTreeLevel'
+	},
+	/**
+	 * Initialize the object
+	 * @param canvas Object HTML canvas DOM element.
+	 * @param [options] map Options for this object.
+	 */
+	initialize: function(canvas, options) {
+		if ($type(canvas) != 'element' || canvas.get('tag') != 'canvas')
+			throw 'The `canvas` argument must be a HTML canvas DOM element.';
+
+		this.canvas = $(canvas);
+		this.setOptions(options);
+	},
+	/**
+	 * Add a single node to the tree
+	 * @param nodes CanvasTreeNode An object which implements CanvasTreeNode.
+	 * @return this
+	 */
+	add_node: function(node) {
+		return this.add_nodes([node]);
+	}, 
+	/**
+	 * Add a collection of nodes to the tree
+	 * @param nodes array<? implements CanvasTreeNode> An array of objects which implement CanvasTreeNode.
+	 * @return this
+	 */
+	add_nodes: function(nodes) {
+		if (this.root == null){
+			this.root = new window[this.options.treeLevelClass](this, null, 0, 0, this.canvas.width, this.canvas.height);
+		}
+		
+		this.root.add_nodes(nodes);
+		return this;
+	},
+	remove_node: function(node){
+		this.node_id_to_level[node.id]._unadopt_node(node);
+		return this;
+	},
+	remove_nodes: function(nodes){
+		nodes.each(this.remove_node, this);
+		return this;
+	}
+});
+
+var CanvasQuadTreeLevel = new Class({
+	x: null,	y: null,
+	w: null,	h: null,
+	
+	owner: null,
+	parent: null,
 	nodes: [],
 	children: {},
-	
-	initialize: function(parent, x, y, w, h){
+	/**
+	 * Initialize the object
+	 * @param owner CanvasTree The owning CanvasTree for this level.
+	 * @param parent CanvasQuadTreeLevel The level above this one in the tree.
+	 * @param x int The x co-ordinate of the top left corner of this level of the tree.
+	 * @param y int The y co-ordinate of the top left corner of this level of the tree.
+	 * @param w int The width of this level of the tree.
+	 * @param h int The height of this level of the tree
+	 */
+	initialize: function(owner, parent, x, y, w, h){
+		this.owner = owner;
 		this.parent = parent;
 		this.x = x; this.w = w;
 		this.y = y; this.h = h;
 	},
-	addNodes: function(nodes){
+	/**
+	 * Add a collection of nodes to the tree
+	 * @param nodes array<? implements CanvasTreeNode> A collection of nodes to add.
+	 */
+	add_nodes: function(nodes) {
 		if (this.w * this.h <= 1 || nodes.length == 1) {
 			for (var i = 0; i < nodes.length; i++){
-				this.nodes.push(nodes[i]);
+				this._adopt_node(nodes[i]);
 			}
 		} else {
 			var childMap = {};
 			for (var i = 0; i < nodes.length; i++) {
-				var child = this.classify(nodes[i]);
+				var child = this._classify(nodes[i]);
 				if (childMap[child] == null)
 					childMap[child] = [];
 				
@@ -50,7 +104,7 @@ var TreeLevel = new Class({
 			for (var child in childMap){
 				if (child == -1) {
 					for (var i = 0; i < childMap[child].length; i++){
-						this.nodes.push(childMap[child][i]);
+						this._adopt_node(childMap[child][i]);
 					}
 				} else {
 					if (childMap[child].length != 0)
@@ -58,12 +112,6 @@ var TreeLevel = new Class({
 				}
 			}
 		}
-	},
-	classify: function(node) {
-		var middleTop = this.x + this.w /2;
-		var middleSide = this.y + this.h /2;
-		
-		return (node.x < middleTop ? 0 : 1) + (node.y < middleSide ? 0 : 2);
 	},
 	addNodesToChildren: function(nodes, child) {
 		if (this.children[child] == null){
@@ -81,6 +129,20 @@ var TreeLevel = new Class({
 			this.children[child] = new this.constructor(this, x, y, this.w /2, this.h /2);
 		}
 		
-		this.children[child].addNodes(nodes);
+		this.children[child].add_nodes(nodes);
+	},
+	_classify: function(node) {
+		var middleTop = this.x + this.w /2;
+		var middleSide = this.y + this.h /2;
+		
+		return (node.x < middleTop ? 0 : 1) + (node.y < middleSide ? 0 : 2);
+	},
+	_adopt_node: function(node) {
+		this.nodes.push(node);
+		this.owner.node_id_to_level[node.id] = this;
+	},
+	_unadopt_node: function(node) {
+		this.nodes.erase(node);
+		delete this.owner.node_id_to_level[node.id];
 	}
 });
--- tests/tree/tree-tests.js
+++ tests/tree/tree-tests.js
@@ -1,11 +1,51 @@
 (new TestSuite(
 	TestCase.create({
-		name: 'Tree',
+		name: 'CanvasTree',
 		
 		test_instantiate: function () {
-			this.assertNothingThrown(function(){
-				new Tree();	
-			});
+			this.assert_throws(function(){	new CanvasTree(); });
+			this.assert_throws(function(){	new CanvasTree(''); });
+			this.assert_throws(function(){	new CanvasTree(5); });
+			this.assert_throws(function(){	new CanvasTree({}); });
+			this.assert_throws(function(){	new CanvasTree([]); });
+			
+			this.assert_nothing_thrown(function(){	new CanvasTree(document.createElement('canvas')); });
+		},
+		test_add_nodes: function() {
+			var canvas = document.createElement('canvas');
+			canvas.width = 1; canvas.height = 1;
+			
+			var tree = new CanvasTree(canvas);
+			var nodes = [new CanvasTreeNode(0, 0, 0), new CanvasTreeNode(1, 1, 0), new CanvasTreeNode(2, 0, 1)];
+			tree.add_nodes(nodes);
+			var node4 = new CanvasTreeNode(3, 1, 1);
+			tree.add_node(node4);
+			
+			nodes.push(node4);
+			this.assert_equal(tree.root.nodes, nodes);
+		},
+		test_add_nodes_tree_depth_limit: function() {
+			var canvas = document.createElement('canvas');
+			canvas.width = 1; canvas.height = 1;
+			
+			var tree = new CanvasTree(canvas);
+			tree.add_nodes([new CanvasTreeNode(0, 0, 0), new CanvasTreeNode(1, 1, 0), new CanvasTreeNode(2, 0, 1), new CanvasTreeNode(3, 1, 1)]);
+			
+			this.assert_equal(tree.root.children, {});
+		}
+	}),
+	TestCase.create({
+		name: 'CanvasQuadTreeLevel',
+		
+		test_instantiate: function() {
+			this.assert_nothing_thrown(function(){	new CanvasQuadTreeLevel(null, 0, 0, 1, 1); });
+		},
+		test__classify: function() {
+			var treeLevel = new CanvasQuadTreeLevel(null, null, 0, 0, 1, 1);
+			this.assert_equal(0, treeLevel._classify(new CanvasTreeNode(0, 0, 0)));
+			this.assert_equal(1, treeLevel._classify(new CanvasTreeNode(0, 1, 0)));
+			this.assert_equal(2, treeLevel._classify(new CanvasTreeNode(0, 0, 1)));
+			this.assert_equal(3, treeLevel._classify(new CanvasTreeNode(0, 1, 1)));
 		}
 	})
 )).run_on_load();
